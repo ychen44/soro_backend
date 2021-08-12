8b45a97219b195ae76db2f98140aa32700340d1b
Ya Chen <yachen@MacBook-Pro.lan>
2021-08-12 13:51:10 -0700

    created user & installed redis packages

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-# mid-tier using mikro-orm, graphql, express, & node 
+# SoRo
 
-## add env file 
+## add env file
+
 ## Install
 
 ```bash
@@ -15,4 +16,10 @@ Copy the `example.env` to `.env` and set the variables
 DB_USER=xxxxx
 DB_NAME=xxxxx
 DB_TYPE= postgresql
+```
+
+## run migration
+
+```
+yarn create:migration
 ```
---
+++ src/resolvers/user.ts
@@ -0,0 +1,123 @@
+import { User } from "../entities/User";
+import { MyContext } from "../types";
+import {
+  Resolver,
+  InputType,
+  Field,
+  Arg,
+  Ctx,
+  Mutation,
+  ObjectType,
+} from "type-graphql";
+import argon2 from "argon2";
+
+@InputType()
+class UsernamePasswordInput {
+  @Field()
+  username: string;
+  @Field()
+  password: string;
+}
+
+@ObjectType()
+class FieldError {
+  @Field()
+  field: string;
+  @Field()
+  message: string;
+}
+
+@ObjectType()
+class UserResponse {
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[];
+
+  @Field(() => User, { nullable: true })
+  user?: User;
+}
+
+@Resolver()
+export class UserResolver {
+  // ********************* Create New Users ************************
+  @Mutation(() => UserResponse)
+  async register(
+    @Arg("options") options: UsernamePasswordInput,
+    @Ctx() { em }: MyContext
+  ): Promise<UserResponse> {
+    if (options.username.length <= 2) {
+      return {
+        errors: [
+          {
+            field: "username",
+            message: "length must be greater than 2 ",
+          },
+        ],
+      };
+    }
+
+    if (options.password.length <= 6) {
+      return {
+        errors: [
+          {
+            field: "password",
+            message: "length must be greater than 6",
+          },
+        ],
+      };
+    }
+
+    const hashedPassword = await argon2.hash(options.password);
+    const user = em.create(User, {
+      username: options.username,
+      password: hashedPassword,
+    });
+    try {
+      await em.persistAndFlush(user);
+    } catch (err) {
+      if (err.code === "23505" || err.detail.includes("already exists")) {
+        errors: [
+          {
+            field: "username",
+            message: "username already exist",
+          },
+        ];
+      }
+      console.log("message: ", err.message);
+    }
+    return { user };
+  }
+
+  // ********************* Create Login ************************
+  @Mutation(() => UserResponse)
+  async login(
+    @Arg("options") options: UsernamePasswordInput,
+    @Ctx() { em }: MyContext
+  ): Promise<UserResponse> {
+    const user = await em.findOne(User, { username: options.username });
+    if (!user) {
+      return {
+        errors: [
+          {
+            field: "username",
+            message: "username does not exist",
+          },
+        ],
+      };
+    }
+    const valid = await argon2.verify(user.password, options.password);
+    if (!valid) {
+      return {
+        errors: [
+          {
+            field: "password",
+            message: "invalid login",
+          },
+        ],
+      };
+    }
+
+    return {
+      user,
+    };
+  }
+}
